17de0a6c95d342025867336eac28ed085c7d60b1
mugwedivhumatshelo <mugwedivhumatshelo@gmail.com>
2025-02-02 20:06:32 -0800

    all files

--- task_5.sql
+++ task_5.sql
@@ -1,3 +1,3 @@
 -- INSERT INTO customer
 INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES (1, 'name', 'mail@h.com', '123 Happiness Ave.');
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
